3a989e1cac516d5f74f07a6ea83f1b2b64a5d08b
ielole <ieloletin@gmail.com>
2017-01-10 07:29:40 -0500

    Add changes after grunt build.
    -Update bundle.js and vendor.bundle.js.

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(3);
-	module.exports = __webpack_require__(57);
+	module.exports = __webpack_require__(58);
 
 
 /***/ },
@@ -10332,7 +10332,7 @@
 
 /***/ },
 
-/***/ 57:
+/***/ 58:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
